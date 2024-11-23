896c8021521a886e1cf8b681c45f12c40749c47f
AbduEhab <abdu.ehab0@gmail.com>
2024-11-23 17:25:48 +0100

    added ImGui and a better example

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,18 @@
-#pragma once
 #define SDL_MAIN_HANDLED
 
 #include <Constants.hpp>
 #include <SDL.h>
 #include <stdio.h>
 
+#include <backends/imgui_impl_sdl2.h>
+#include <backends/imgui_impl_sdlrenderer2.h>
+#include <imgui.h>
+
 // uncomment the following lines if you want to use the SDL libraries (must be enabled in the CMakeLists.txt!!!)
 // #include <SDL_image.h>
 // #include <SDL_mixer.h>
 // #include <SDL_ttf.h>
 // #include <SDL_net.h>
-
-enum class Cycle
-{
-	UP,
-	DOWN
-};
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *arg[])
 {
@@ -47,79 +44,87 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *arg[])
 		return 1;
 	}
 
-	uint8_t i = 0, j = 0, k = 0;
-	Cycle cycle = Cycle::UP;
+	SDL_GL_SetSwapInterval(1); // Enable vsync
 
-	// cycle rainbow colors to show that the window is working
+	// init imgui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	(void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+	ImGui_ImplSDLRenderer2_Init(renderer);
+
+	// vars for the bouncing rectangle
+	int x = 60;
+	int y = 60;
+	int x_vel = 300;
+	int y_vel = 300;
+
+	TimePoint end_time_of_last_frame = Clock::now();
+	float delta_time = 0.0f;
 	while (true)
 	{
+		// Start the ImGui frame
+		ImGui_ImplSDLRenderer2_NewFrame();
+		ImGui_ImplSDL2_NewFrame();
+		ImGui::NewFrame();
+		ImGui::Begin("Hello, world!");
+		ImGui::Text("Template Created by: %s", "AbduEhab");
+		ImGui::End();
+
 		// Clear the window to black
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
-		// Draw a rainbow rectangle
-		SDL_Rect rect;
-		rect.x = 0;
-		rect.y = 0;
-		rect.w = 640;
-		rect.h = 480;
+		// Draw a rectangle
+		SDL_Rect rect(x, y, 60, 60);
 
-		SDL_SetRenderDrawColor(renderer, i, j, k, 255);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+		// have rectangle bounce around the screen
+
+		x += x_vel * delta_time;
+		y += y_vel * delta_time;
+
+		if (x >= 580 || x <= 0)
+		{
+			x_vel *= -1;
+		}
+
+		if (y >= 420 || y <= 0)
+		{
+			y_vel *= -1;
+		}
+
+		rect.x = x;
+		rect.y = y;
+
 		SDL_RenderFillRect(renderer, &rect);
+
+		// Render the ImGui frame
+		ImGui::Render();
+		ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
 
 		// Update the window
 		SDL_RenderPresent(renderer);
 
-		// Cycle the colors
-		switch (cycle)
-		{
-		case Cycle::UP:
-			if (++i == 255)
-			{
-				j++;
-				i = 254;
-			}
-			if (j == 255)
-			{
-				k++;
-				j = 254;
-			}
-			if (k == 255)
-			{
-				cycle = Cycle::DOWN;
-			}
-			break;
-		case Cycle::DOWN:
-			if (--i == 0)
-			{
-				j--;
-				i = 1;
-			}
-			if (j == 0)
-			{
-				k--;
-				j = 1;
-			}
-			if (k == 0)
-			{
-				cycle = Cycle::UP;
-			}
-
-		default:
-			break;
-		}
-
 		// Check for quit event
 		SDL_Event event;
-		if (SDL_PollEvent(&event))
+		SDL_PollEvent(&event);
 		{
+			ImGui_ImplSDL2_ProcessEvent(&event);
+
 			if (event.type == SDL_QUIT)
 			{
 				break;
 			}
 		}
 
-		SDL_Delay(5);
+		delta_time = (Clock::now() - end_time_of_last_frame).count() * 1e-9;
+		end_time_of_last_frame = Clock::now();
+
+		print(delta_time);
 	}
 
 	// Destroy the window and shutdown the program
